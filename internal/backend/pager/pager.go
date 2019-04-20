a84dd30347d8929ccdedabcf68292c4d1b0fd4b2
angles-n-daemons <dillmann.brian@gmail.com>
2019-04-20 16:36:36 -0400

    add Create and addHeader functions to pager

--- internal/backend/pager/pager.go
+++ internal/backend/pager/pager.go
@@ -17,6 +17,7 @@ Properties
 	 - pageSize:      the number of bytes in each data page
 	 - fs:            file system operator used for reading and writing to the db
 	 - freelistIndex: the page number where the freelist starts
+	 - freelistNum:   the number of free pages available to the pager
 
 	 The database has the following pages:
 
@@ -28,6 +29,7 @@ type Pager struct {
 	pageSize      int
 	fs            file
 	freelistIndex int
+	freeListNum   int
 }
 
 /*
@@ -68,4 +70,30 @@ func (p *Pager) Set(offset int, content []byte) error {
 
 	offsetBytes := (offset * p.pageSize) + (RESERVED_HEADER_PAGE_COUNT * p.pageSize)
 	return p.fs.Write(offsetBytes, content)
+}
+
+/*
+	Create performs the following actions under lock:
+	1. writes the content to the first freelist page
+	2. bumps the value of the freelist index
+	3. sets a success property
+	4. if something fails in between, decrements the freelist index
+*/
+func (p *Pager) Create([]byte) (int, error) {
+	return 0, nil
+}
+
+/*
+	writeHeader is used to write metadata changes about the
+	database to disk so that it can be read in on a crash
+*/
+func (p *Pager) writeHeader([]byte) error {
+	headerBytes = make([]byte, 0, p.pageSize)
+	headerBytes = append(headerBytes, []byte("POPSQL_1"))
+
+	freelistIndexBytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(keyBytes, cell.key)
+
+	freelistNumBytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(freelistNumBytes)
 }
