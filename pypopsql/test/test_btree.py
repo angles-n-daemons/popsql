1a9d5e42afc9dc59f564fe4554c50106a268e2d8
Brian Dillmann <dillmann.brian@gmail.com>
2023-07-29 18:35:45 -0400

    fix varint test

--- pypopsql/test/test_btree.py
+++ pypopsql/test/test_btree.py
@@ -17,8 +17,7 @@ class TestVarint(TestCase):
             VarIntTestCase([0x7f], 0x0000007f, 1),
             VarIntTestCase([0x81, 0x00], 0x00000080, 2),
             VarIntTestCase([0x80, 0x7f], 0x0000007f, 2),
-            # test case broken, pending sqlite authors discussion
-            # VarIntTestCase([0x8a, 0x91, 0xd1, 0xac, 0x78], 0x12345678, 5),
+            VarIntTestCase([0x81, 0x91, 0xd1, 0xac, 0x78], 0x12345678, 5),
             VarIntTestCase([0x81, 0x81, 0x81, 0x81, 0x01], 0x10204081, 5),
         ]
         for test in tests:
