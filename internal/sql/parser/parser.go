78cbc1d3083400d06240fba42820e9f13388d9c2
Brian Dillmann <dillmann.brian@gmail.com>
2024-08-28 23:03:00 -0400

    first pass on grammar

--- internal/sql/parser/parser.go
+++ internal/sql/parser/parser.go
@@ -1,26 +1,36 @@
 package parser
 
 /*
-statement       → insert | select | update | delete
+statement       → insert | select | update | delete | create
 
-select          → "SELECT" + expression_list +
-                  (+ "FROM"  table_expr)?
-                  (+ "WHERE" logic_or)?
-                  (+ "GROUP BY" expression_list)?
-                  (+ "OFFSET" expression)?
-                  (+ "LIMIT" expression)?
+create          → "CREATE TABLE" table "("
+		   column_spec ( "," column_spec)*
+                   ")"
 
-insert          → "INSERT INTO " + table + parameters
-                  ("VALUES" + tuple + ("," + tuple)*) |
-                  select
+select          → "SELECT" expression_list
+                  ( "FROM" table_expr)?
+                  ( "WHERE" logic_or)?
+                  ( "GROUP BY" expression_list)?
+                  ( "OFFSET" expression)?
+                  ( "LIMIT" expression)?
+
+insert          → "INSERT INTO " table parameters
+                  ("VALUES" tuple ("," tuple)*) | select
+
+column          → identifier type
+type            → "integer" | "varchar" | "boolean"
 
 expression      → or
-or              → and + "OR" + and
-and             → equality + "AND" + equality
-equality
-comparison
+or              → and "OR" and
+and             → equality "AND" equality
+equality        → comparison ( ( "!=" | "==" ) comparison)*
+comparison      → term ( ( ">" | ">=" | "<" | "<=" ) term)*
+term            → factor ( ( "-" | "+" ) factor )*
+factor          → unary ( ( "/" | "*" ) unary)*
+primary         → "true" | "false" | "nil" |
+                  NUMBER | STRING | IDENTIFIER | "(" expression ")"
 
-expression_list → expression + ("," + expression)*
-parameters      → identifier + ("," + identifier)*
-tuple           → "(" + expression_list + ")"
+expression_list → expression  (","  expression)*
+parameters      → identifier  (","  identifier)*
+tuple           → "("  expression_list  ")"
 */
